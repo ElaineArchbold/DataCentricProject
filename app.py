71861a406ada8d0e1def63979dc287d5b041586b
Elaine Archbold <e.t.archbold@gmail.com>
2020-02-10 10:53:39 +0000

    setting environment variables in Heroku

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 
 app = Flask(__name__)
 
+app.config["MONGODB_URI"] = os.getenv("MONGO_URI")
+app.config["DBS_NAME"] = "HinchingJournal"
+mongo = PyMongo(app)
+
 
 @app.route('/')
-def hello():
-    return "hello there you beaut"
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("todo.html", tasks=mongo.db.tasks.find())
 
 
 if __name__ == '__main__':
